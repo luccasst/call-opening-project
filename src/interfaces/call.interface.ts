5a2f1c1bb359f80084a942f91688c346155ee2b1
luccasst <lucas.c.r.f@hotmail.com>
2023-04-14 12:47:57 -0300

    add interfaces

---
+++ src/interfaces/call.interface.ts
@@ -0,0 +1,11 @@
+export default interface Call {
+    id: number;
+    title: string;
+    comment: string;
+    status: string;
+    user: number;
+    priority: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  
